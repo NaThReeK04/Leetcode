22fa5f96bbd97a67bc2530f018735f04d5525d90
Keerthan Ghodiwal <151128097+keerthan1304@users.noreply.github.com>
2024-09-29 14:37:54 +0530

    Time: 2 ms (81.25%), Space: 16.9 MB (28.42%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
         for(int i=0;i<nums.size();i++){
             if(nums[i]==target){
                 return true;
