a9331103f3f831a92086d1c99c155bd6569ec0b0
Keerthan Ghodiwal <151128097+keerthan1304@users.noreply.github.com>
2024-10-05 11:25:45 +0530

    Time: 913 ms (8.55%), Space: 10 MB (39.48%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,35 +1,47 @@
-class Solution {
-public:
-    bool checkInclusion(string s1, string s2) {
-        int n=s1.length();
-        int m=s2.length();
-        if (n > m) return false;
-        unordered_map<char,int>mpp;
-        for(int i=0;i<n;i++){
-            mpp[s1[i]]++;
-        }
-        unordered_map<char,int>temp;
-        temp=mpp;
-        for(int i=0;i<=m-n;i++){
-            for(int j=i;j<i+n;j++){
-                if(mpp.find(s2[j])!=mpp.end()){
-                    mpp[s2[j]]--;
+class Solution
+{
+    public:
+        bool checkInclusion(string s1, string s2)
+        {
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+            int n = s1.length();
+            int m = s2.length();
+            if (n > m) return false;
+            unordered_map<char, int> mpp;
+            for (int i = 0; i < n; i++)
+            {
+                mpp[s1[i]]++;
+            }
+            unordered_map<char, int> temp;
+            temp = mpp;
+            for (int i = 0; i <= m - n; i++)
+            {
+                for (int j = i; j < i + n; j++)
+                {
+                    if (mpp.find(s2[j]) != mpp.end())
+                    {
+                        mpp[s2[j]]--;
+                    }
+                }
+                bool we = false;
+                for (auto it: mpp)
+                {
+                    if (it.second != 0)
+                    {
+                        we = true;
+                        break;
+                    }
+                }
+                if (we == true)
+                {
+                    mpp = temp;
+                }
+                else
+                {
+                    return true;
                 }
             }
-            bool we=false;
-            for(auto it:mpp){
-                if(it.second!=0){
-                    we=true;
-                    break;
-                }
-            }
-            if(we==true){
-                mpp=temp;
-            }else{
-                return true;
-            }
+            return false;
         }
-        return false;
-
-    }
 };
