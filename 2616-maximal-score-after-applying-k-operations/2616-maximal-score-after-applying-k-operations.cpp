9e844f3910fe29294cab931ab07293a0e2111371
Keerthan Ghodiwal <151128097+keerthan1304@users.noreply.github.com>
2024-10-14 19:15:53 +0530

    Time: 194 ms (60.6%), Space: 79.8 MB (50.62%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -1,24 +1,20 @@
-#include <cmath>
-#include <vector>
-#include <queue>
-using namespace std;
-
+#include<cmath>
 class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
-        int n = nums.size();
+        int n=nums.size();
         priority_queue<int> pq;
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             pq.push(nums[i]);
         }
-        long long sum = 0;
-        for (int i = 0; i < k; i++) {
-            int t = pq.top();
+        long long sum=0;
+        for(int i=0;i<k;i++){
+            int t=pq.top();
             pq.pop();
-            sum += t;
-            t = ceil((double)t / 3);
+            sum+=t;
+            t=ceil((double)t/3);
             pq.push(t);
         }
-        return sum;
+        return sum;   
     }
 };
