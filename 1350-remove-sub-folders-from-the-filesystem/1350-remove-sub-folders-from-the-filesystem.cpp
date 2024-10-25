592f36639010db91a7f228eb2d35d097ad6f8b46
Keerthan Ghodiwal <151128097+keerthan1304@users.noreply.github.com>
2024-10-25 14:40:45 +0530

    Time: 302 ms (21.43%), Space: 74.7 MB (46.98%) - LeetHub

---
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,60 @@
+class Solution
+{
+    public:
+        vector<string> removeSubfolders(vector<string> &folder)
+        {
+            vector<string> ans;
+            unordered_map<string, int> mpp;
+            sort(folder.begin(),folder.end());
+            for(int i=0;i<folder.size();i++){
+                cout<<folder[i]<<"  ";
+            }
+            int n = folder.size();  
+            for (int i = 0; i < n; i++)
+            {
+                int count1 = 1;
+                int count2 = 0;
+                string k = folder[i];
+                for (int j = 1; j < k.length(); j++)
+                {
+                    if (k[j] != '/')
+                    {
+                        count1++;
+                    }
+                    else if (k[j] == '/')
+                    {
+                        break;
+                    }
+                }
+                for (int l = k.length() - 1; l >= 0; l--)
+                {
+                    if (k[l] != '/')
+                    {
+                        count2++;
+                    }
+                    else if (k[l] == '/')
+                    {
+                        break;
+                    }
+                }
+                count2++;
+                bool ye=false;
+                for(int t=count2;t<=folder[i].length();t+=count2){
+                    string q = folder[i].substr(0, folder[i].length() - t);
+                    if(mpp.find(q)!=mpp.end()){
+                        ye=true;
+                        break;
+                    }
+                }
+
+                string s = folder[i].substr(0, folder[i].length() - count2);
+                string p = folder[i].substr(0, count1);
+                if (mpp.find(s) == mpp.end() && mpp.find(p) == mpp.end()&&ye==false)
+                {
+                    ans.push_back(folder[i]);
+                    mpp[folder[i]] = 1;
+                }
+            }
+            return ans;
+        }
+};
