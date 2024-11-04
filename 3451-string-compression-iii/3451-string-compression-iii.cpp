37ef8df1b7e43d937b28a9fcc2ea259c336a7468
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2024-11-04 21:17:38 +0530

    Time: 29 ms (66.67%), Space: 28 MB (77.38%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<word.size();i++){
             v[word[i]-'a']++;
             if(word[i]!=word[i+1]||v[word[i]-'a']==9){
-                comp+=to_string(v[word[i]-'a']);
+                comp+=(v[word[i]-'a']+'0');
                 comp+=word[i];
                 v[word[i]-'a']=0;
             }
