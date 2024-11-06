2efd35d5748009439a7e1edae28aa10e4c33b829
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2024-11-06 20:35:21 +0530

    Time: 18 ms (10.07%), Space: 32 MB (25.8%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  int printBits(int num) {
+    int count=0;
+    for (int i = 31; i >= 0; --i) {
+        count+=((num >> i) & 1);
+    }
+    return count;
+}
+    bool canSortArray(vector<int>& nums) {
+        vector<int>temp=nums;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]&&printBits(nums[j])==printBits(nums[j+1])){
+                    swap(nums[j],nums[j+1]);
+                }
+            }
+        }
+        sort(temp.begin(),temp.end());
+        if(temp==nums)return true;
+        return false;
+
+        
+    }
+};
