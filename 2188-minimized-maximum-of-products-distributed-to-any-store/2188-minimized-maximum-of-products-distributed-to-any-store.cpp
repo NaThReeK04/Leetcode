ca8dbbc7b187b55225e0fbc5937c85f22b30fa75
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2024-11-14 21:34:52 +0530

    Time: 16 ms (99.25%), Space: 87.2 MB (89.12%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int left=1,right=100000;
+        while(left<right){
+            int mid=(left+right)/2,sum=0;
+            for(auto a:quantities){
+                sum+=(a+mid-1)/mid;
+            }
+            if(sum>n){
+                left=mid+1;
+            }else{
+                right=mid;
+            }
+        }
+        return left;
+    }
+};
