efc00c1497ced634b8742c8cbd20edb7fe6da54a
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2024-11-16 12:54:14 +0530

    Time: 0 ms (100%), Space: 7.6 MB (11.65%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(vector<int>&dp,int n){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=solve(dp,n-1)+solve(dp,n-2);
+    }
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        int ans=solve(dp,n);
+        return ans;
+    }
+};
