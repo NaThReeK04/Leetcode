5a4f9d9bc31c61909894e306aae24443a8815454
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2024-12-01 18:21:40 +0530

    Time: 256 ms (36.36%), Space: 204.9 MB (27.27%) - LeetHub

---
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int bfs(int idx, int n, vector<vector<int>>& adj, int limit) {
+        if (limit < 0) return 1; 
+        
+        vector<int> vis(n + 1, 0);
+        queue<int> q;
+        q.push(idx);
+        vis[idx] = 1;
+        int count = 0, nodes = 0;
+
+        while (!q.empty() && count <= limit) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                int node = q.front();
+                q.pop();
+                nodes++;
+                for (auto& neighbor : adj[node]) {
+                    if (!vis[neighbor]) {
+                        vis[neighbor] = 1;
+                        q.push(neighbor);
+                    }
+                }
+            }
+            count++;
+        }
+        return nodes;
+    }
+
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
+        int n = edges1.size();
+        int m = edges2.size();
+
+        vector<vector<int>> adj1(n + 1), adj2(m + 1);
+        for (const auto& edge : edges1) {
+            adj1[edge[0]].push_back(edge[1]);
+            adj1[edge[1]].push_back(edge[0]);
+        }
+        for (const auto& edge : edges2) {
+            adj2[edge[0]].push_back(edge[1]);
+            adj2[edge[1]].push_back(edge[0]);
+        }
+
+        if (k == 0) {
+            vector<int> result(n + 1, 1);
+            return result;
+        }
+
+        vector<int> dist1(n + 1), dist2(m + 1);
+        for (int i = 0; i <= n; ++i) {
+            dist1[i] = bfs(i, n, adj1, k);
+        }
+        for (int j = 0; j <= m; ++j) {
+            dist2[j] = bfs(j, m, adj2, k - 1);
+            if (dist2[j] == 0) dist2[j] = 1; 
+        }
+
+        int maxi = *max_element(dist2.begin(), dist2.end());
+        vector<int> ans(n + 1);
+        for (int i = 0; i <= n; ++i) {
+            ans[i] = dist1[i] + maxi;
+        }
+
+        return ans;
+    }
+};
