0908a59810d6f71e8c42cb3cf11cac13b044b7dc
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2024-12-02 18:58:05 +0530

    Time: 13 ms (24.99%), Space: 48.1 MB (32.67%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int solve(int child, int greed, vector<int>& g, vector<int>& s, vector<int>& dp) {
-        if (child < 0 || greed < 0) return 0;
-        if (dp[greed] != -1) return dp[greed];
-
-        return dp[greed] = (g[child] <= s[greed]) 
-            ? 1 + solve(child - 1, greed - 1, g, s, dp) 
-            : solve(child - 1, greed, g, s, dp);
+    int solve(int child,int greed,vector<int>&g,vector<int>&s,vector<int>&dp){
+        if(child<0||greed<0)return 0;
+        if(dp[greed]!=-1)return dp[greed];
+        return (g[child]<=s[greed])?dp[greed]=1+solve(child-1,greed-1,g,s,dp):dp[greed]=solve(child-1,greed,g,s,dp);
     }
-
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n = g.size();
-        int m = s.size();
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-
-        vector<int> dp(m + 1, -1); 
-
-        return solve(n - 1, m - 1, g, s, dp);
+        int n=g.size();
+        int m=s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        vector<int>dp(m+1,-1);
+        return solve(n-1,m-1,g,s,dp);
     }
 };
