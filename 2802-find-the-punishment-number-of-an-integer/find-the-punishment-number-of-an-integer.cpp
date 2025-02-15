e03752d448af7bf744ecaadff822cac2fab399c2
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-02-15 17:52:59 +0530

    Time: 1435 ms (5.04%) | Memory: 11.3 MB (45.09%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    bool isPossible(int i, string t, string s, int n, int target) {
-        if (i == n) {
-            int sum1 = 0;
-            string temp = "";
-            for (int j = 0; j < t.length(); j++) {
-                if (isdigit(t[j])) {
-                    temp += t[j];
-                } else if (t[j] == '+') {
-                    sum1 += stoi(temp);
+    bool isPossible(int i,string t,string s,int n,int target){
+        if(i==n){
+            int sum1=0;
+            string temp="";
+            for(int j=0;j<t.length();j++){
+                if(isdigit(t[j])){
+                    temp+=t[j];
+                }else if(t[j]=='+'){
+                    sum1+=stoi(temp);
                     temp.clear();
                 }
             }
-            if (!temp.empty()) {
-                sum1 += stoi(temp);
-            }
-            return sum1 == target;
+            if(!temp.empty()){
+                    sum1+=stoi(temp);
+             }
+            return sum1==target;
+
         }
-
-        bool ans1 = isPossible(i + 1, t + string(1, s[i]) + "+", s, n, target);
-        bool ans2 = isPossible(i + 1, t + string(1, s[i]), s, n, target);
-        return ans1 || ans2;
+        bool ans1=isPossible(i+1,t+string(1,s[i])+"+",s,n,target);
+        bool ans2=isPossible(i+1,t+string(1,s[i]),s,n,target);
+        return ans1||ans2;
     }
-
     int punishmentNumber(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            string s = to_string(i * i);
-            int length = s.length();
-            if (isPossible(0, "", s, length, i)) {
-                sum += (i * i);
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            string s=to_string(i*i);
+            int n=s.length();
+            string t="";
+            if(isPossible(0,t,s,n,i)){
+                sum+=(i*i);
             }
         }
         return sum;
