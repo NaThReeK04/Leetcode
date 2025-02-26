c6ad6176dbaa56cd416a455376e1236d907d63d6
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-02-27 01:23:03 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (84.48%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,34 +3,27 @@ public:
     int majorityElement(vector<int>& nums) {
         int ele;
         int count = 0;
-
-        // Step 1: Find the candidate
-        for (int i = 0; i < nums.size(); i++) {
-            if (count == 0) {
-                ele = nums[i];  // Select new candidate when count is zero
+        for(int i = 0; i < nums.size(); i++) {
+            if(count == 0) {
+                ele = nums[i];
                 count = 1;
-            } 
-            else if (nums[i] == ele) {
-                count++;  // Increase count if same as candidate
-            } 
-            else {
-                count--;  // Decrease count if different from candidate
+            }
+            else if(nums[i] == ele) {
+                count++;
+            }
+            else{
+                count--;
             }
         }
-
-        // Step 2: Verify the candidate
         int cnt = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == ele) {
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == ele) {
                 cnt++;
             }
         }
-
-        // If candidate occurs more than half the time, return it, otherwise return -1
-        if (cnt > (nums.size() / 2)) {
+        if(cnt > ceil(nums.size() / 2)) {
             return ele;
         }
-        
         return -1;
     }
 };
