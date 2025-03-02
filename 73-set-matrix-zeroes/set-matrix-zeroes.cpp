9733bb7102804039b91bb77b1390cf47f95a4061
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-03-02 23:11:53 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (52.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int>row;
-        set<int>col;
+        int col0=1;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
                 if(matrix[i][j]==0){
-                    row.insert(i);
-                    col.insert(j);
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }else{
+                        col0=0;
+                    }
                 }
             }
         }
-        for(auto it:row){
-            for(int i=0;i<matrix[0].size();i++){
-                matrix[it][i]=0;
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++){
+                if(matrix[i][0]==0||matrix[0][j]==0)matrix[i][j]=0;
             }
         }
-        for(auto it:col){
+        if(matrix[0][0]==0){
+            for(int j=0;j<matrix[0].size();j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0==0){
             for(int i=0;i<matrix.size();i++){
-                matrix[i][it]=0;
+                matrix[i][0]=0;
             }
         }
+        return;
     }
 };
