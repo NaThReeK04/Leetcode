79ed2ac7e1fcd188cbdae3a7e2123ac4916cd038
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-03-03 21:17:35 +0530

    Time: 11 ms (31.49%) | Memory: 127.7 MB (77.25%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+        vector<int>ans(n);
+        int less=0;
+        int equal=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot)less++;
+            else if(nums[i]==pivot)equal++;
+        }
+        int lidx=0;
+        int eidx=less;
+        int hidx=less+equal;
+        for(int i=0;i<n;i++){
             if(nums[i]<pivot){
-                ans.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==pivot){
-                ans.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>pivot){
-                ans.push_back(nums[i]);
+                ans[lidx]=nums[i];
+                lidx++;
+            }else if(nums[i]>pivot){
+                ans[hidx]=nums[i];
+                hidx++;
+            }else{
+                ans[eidx]=nums[i];
+                eidx++;
             }
         }
         return ans;
-
     }
 };
