fbf1bab74b13ce2169dac70e15f689d556c9a378
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-08-13 20:30:05 +0530

    Time: 0 ms (100%), Space: 14.7 MB (57.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
             int search=target-nums[i];
-            if(mpp.find(search)!=mpp.end()&&mpp[search]!=i)return {i,mpp[search]};
+            if(mpp.find(search)!=mpp.end()&&mpp[nums[i]]!=i)return {i,mpp[search]};
             mpp[nums[i]]=i;
         }
         return {-1,-1};
