f67b361b893c1b6ca384dc749cda6bf1508e3ba0
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-09-03 19:30:34 +0530

    Time: 0 ms (100%), Space: 52.3 MB (34.86%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.size()==1)return true;
-        if(nums[0]==0)return false;
-        int goal=nums.size()-1;
-        int reach=0;
-        for(int i=0;i<nums.size();i++){
-            int tempReach=i+nums[i];
-            if(tempReach==i&&reach<=i)return false;
-            if(tempReach>reach){
-                reach=tempReach;
-            }
-            if(reach>=goal&&i!=nums.size()-1)return true;
+        int reach = 0;
+        int goal = nums.size() - 1;
+
+        for (int i = 0; i <= reach; i++) {
+            reach = max(reach, i + nums[i]);
+            if (reach >= goal) return true;
         }
+
         return false;
     }
 };
