f1dff5750182b2bfb5f471e7a6cf2ed0719db827
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-09-03 19:51:22 +0530

    Time: 23 ms (29.36%), Space: 22 MB (13.02%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int helper(vector<int>&nums,int goal){
-        if(goal==0)return 0;
-        int num;
-        int reach=0;
-        for(int i=0;i<goal;i++){
-            reach=max(reach,i+nums[i]);
-            if(reach>=goal){
-                num=i;
+    int helper(vector<int>& nums, int goal, vector<int>& dp) {
+        if (goal == 0) return 0;
+        if (dp[goal] != -1) return dp[goal];
+
+        int num = 0;
+        int reach = 0;
+
+        for (int i = 0; i < goal; i++) {
+            reach = max(reach, i + nums[i]);
+            if (reach >= goal) {
+                num = i;
                 break;
             }
         }
-        return 1+helper(nums,num);
+
+        return dp[goal] = 1 + helper(nums, num, dp);
     }
+
     int jump(vector<int>& nums) {
-        return helper(nums,nums.size()-1);
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return helper(nums, n - 1, dp);
     }
 };
