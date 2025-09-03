7fbe3247e424a47cc5fa8451652d6dfd453cfeb3
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-09-03 18:23:02 +0530

    Time: 40 ms (29.37%), Space: 47.6 MB (31.8%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int> freq;
-        for(char t : tasks) freq[t]++;
-
-        // last execution time for each task (initialize to -inf)
-        vector<int> lastExec(26, -100000);
-
+        vector<int> freq(26, 0);
+        for (int i = 0; i < tasks.size(); i++) {
+            freq[tasks[i] - 'A']++;
+        }
+        priority_queue<int> pq;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0)
+                pq.push(freq[i]);
+        }
+        
         int time = 0;
-        int remaining = tasks.size();
-
-        while(remaining > 0) {
-            char bestTask = '#';
-            // find best task to execute at this time
-            for(auto &p : freq) {
-                char task = p.first;
-                int count = p.second;
-                if(count > 0 && time - lastExec[task - 'A'] > n) {
-                    if(bestTask == '#' || freq[task] > freq[bestTask]) {
-                        bestTask = task;
-                    }
+        while (!pq.empty()) {
+            vector<int> temp;
+            for (int i = 1; i <= n + 1; i++) {
+                if (!pq.empty()) {
+                    int num = pq.top();
+                    pq.pop();
+                    num--;
+                    temp.push_back(num);
                 }
             }
-
-            if(bestTask == '#') {
-                // no task can be scheduled -> idle
-                time++;
+            for (int i = 0; i < temp.size(); i++) {
+                if (temp[i] != 0)
+                    pq.push(temp[i]);
+            }
+            if (pq.empty()) {
+                time += temp.size();
             } else {
-                // schedule bestTask
-                freq[bestTask]--;
-                lastExec[bestTask - 'A'] = time;
-                time++;
-                remaining--;
+                time += n + 1;
             }
         }
         return time;
