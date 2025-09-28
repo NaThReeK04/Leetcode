2d872ed6b5248c439271a5572c28605a4e68cf3e
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-09-28 13:54:54 +0530

    Time: 152 ms (34.71%), Space: 205.6 MB (6.85%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    bool helper(vector<int>&nums,vector<vector<int>>&dp,int n,int sum){
-        if(n==0){
-            if(sum==0)return true;
-            return false;
+    void helper(vector<int>& nums, vector<vector<int>>& dp, int n, int sum) {
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
-        if(dp[n][sum]!=-1)return dp[n][sum];
-        if(nums[n-1]<=sum){
-            return dp[n][sum]=helper(nums,dp,n-1,sum-nums[n-1])||helper(nums,dp,n-1,sum);
-        }else if(nums[n-1]>sum){
-            return dp[n][sum]=helper(nums,dp,n-1,sum);
-        }
-        return false;
     }
     bool canPartition(vector<int>& nums) {
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%2!=0)return false;
-        int target=sum/2;
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return helper(nums,dp,n,target);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2 != 0)
+            return false;
+        int target = sum / 2;
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+        for (int i = 0; i < n + 1; i++)
+            dp[i][0] = 1;
+        helper(nums, dp, n, target);
+        return dp[n][target];
     }
 };
