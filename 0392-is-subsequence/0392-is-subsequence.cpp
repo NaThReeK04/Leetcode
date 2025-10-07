2782522301bc73dde3493de39d5bc8d5bf656250
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-10-07 23:26:45 +0530

    Time: 3 ms (5.71%), Space: 10.6 MB (5.44%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,18 +3,16 @@ public:
     bool isSubsequence(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<string>>dp(n+1,vector<string>(m+1,""));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+s[i-1];
+                    dp[i][j]=dp[i-1][j-1]+1;
                 }else{
-                    string temp1=dp[i-1][j];
-                    string temp2=dp[i][j-1];
-                    dp[i][j]=(temp1>temp2)?temp1:temp2;
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        return dp[n][m]==s;
+        return dp[n][m]==s.size();
     }
 };
