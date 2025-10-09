0374c57bfa1f0bdaea89014b40de2345ed771d3e
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-10-09 12:52:39 +0530

    Time: 0 ms (100%), Space: 14.1 MB (88.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int helper(vector<int>&nums,int idx,int prev_idx,vector<vector<int>>&dp,int n){
-        if(idx==n)return 0;
-        if(dp[idx][prev_idx+1]!=-1)return dp[idx][prev_idx+1];
-        int notTake=helper(nums,idx+1,prev_idx,dp,n);
-        int Take=INT_MIN;
-        if(prev_idx==-1||nums[idx]>nums[prev_idx]){
-            Take=1+helper(nums,idx+1,idx,dp,n);
-        }
-        return dp[idx][prev_idx+1]=max(notTake,Take);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return helper(nums,0,-1,dp,n);
+        //binary search approch
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+            }else{
+                int idx=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[idx]=nums[i];
+            }
+        }
+        return temp.size();
     }
 };
