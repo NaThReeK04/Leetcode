a1db979893fc188b9b832ffffcb916d17a7666d4
Keerthan Ghodiwal <151128097+NaThReeK04@users.noreply.github.com>
2025-10-19 17:44:42 +0530

    Time: 22 ms (57.06%), Space: 17.5 MB (26.98%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,14 +4,14 @@ public:
         vector<vector<int>>ans;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n-3;i++){
             if(i>0&&nums[i]==nums[i-1])continue;
-            for(int j=i+1;j<n;j++){
+            for(int j=i+1;j<n-2;j++){
                 if(j>i+1&&nums[j]==nums[j-1])continue;
                 int k=j+1;
                 int l=n-1;
                 while(k<l){
-                    long long sum=nums[i]+nums[j]+nums[k]+nums[l];
+                    long long sum=(long long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum==target){
                         ans.push_back({nums[i],nums[j],nums[k],nums[l]});
                         k++;
